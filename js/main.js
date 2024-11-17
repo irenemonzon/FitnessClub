b8d6de11798b1d8524a6f962c49bbdc66b5809c0
irenemonzon <irene.monzonm@gmail.com>
2024-11-17 21:59:29 +1100

    create section membership, staff and contact and styles

---
+++ js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar-container');
+  const navbarOptions = document.querySelector('.navbar-options');
+
+
+  if (window.scrollY > 50 && !navbarOptions.classList.contains('open')) { 
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbarOptions = document.querySelector('.navbar-options');
+
+hamburgerMenu.addEventListener('click', function() {
+  navbarOptions.classList.toggle('open');
+ 
+  const navbar = document.querySelector('.navbar-container');
+  if (navbarOptions.classList.contains('open')) {
+    navbar.classList.remove('navbar-scrolled');
+  }else {
+    navbar.classList.add('navbar-scrolled');
+  }
+});
+
+const currentUrl = window.location.href;
+
+const navbarLinks = document.querySelectorAll('.navbar-options li a');
+
+
+navbarLinks.forEach(link => {
+  console.log(link.href )
+   
+    if (link.href === currentUrl) {
+      console.log('entra aqui')
+        link.classList.add('active');
+    }
+})
